a2660efa47df1077e8dfb74222f28933cba28326
cjj <1046696528@qq.com>
2024-04-12 11:55:45 +0800

    添加抽奖过程功能

--- lottery-domain/src/main/java/com/jun/lottery/domain/strategy/service/draw/AbstractDrawBase.java
+++ lottery-domain/src/main/java/com/jun/lottery/domain/strategy/service/draw/AbstractDrawBase.java
@@ -45,7 +45,6 @@ public abstract class AbstractDrawBase extends DrawStrategySupport implements ID
 
     /**
      * 获取不在抽奖范围内的列表，包括：奖品库存为空、风控策略、临时调整等，这类数据是含有业务逻辑的，所以需要由具体的实现方决定
-     *
      * @param strategyId 策略ID
      * @return 排除的奖品ID集合
      */
